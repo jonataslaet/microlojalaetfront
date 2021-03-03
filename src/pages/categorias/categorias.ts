f56dd103387a55cfecb802d08b519c39a9f0346a
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-03 06:21:01 -0300

    Listadas as categorias na tela de Categorias os links das imagens são do bucket da amazon

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
@@ -16,17 +18,23 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public categoriaService: CategoriaService) {
    
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(r => {
-      console.log(r);
+    this.categoriaService.findAll().subscribe(response => {
+      this.items = response;
     },
     error => {
       console.log(error);
-    })
+    });
   }
 
 }
