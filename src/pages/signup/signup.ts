56edbe1ab275ae59d9e387d44dbeae9274fc4d56
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-06 11:01:40 -0300

    Povoada dinamicamente o estado e as suas cidades

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CidadeDTO } from '../../models/cidade.dto';
+import { EstadoDTO } from '../../models/estado.dto';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
 
 @IonicPage()
 @Component({
@@ -10,11 +14,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   formGroup: FormGroup;
+  cidades: CidadeDTO[];
+  estados: EstadoDTO[];
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public formBuider: FormBuilder) {
+    public formBuider: FormBuilder,
+    public cidadeService : CidadeService,
+    public estadoService: EstadoService) {
       this.formGroup = this.formBuider.group({
         nome: ['Teste',[Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
         email: ['teste@gmail.com',[Validators.required, Validators.email]],
@@ -32,6 +40,27 @@ export class SignupPage {
         estadoId:[null, [Validators.required]],
         cidadeId:[null, [Validators.required]]
       });
+  }
+
+
+  ionViewDidLoad(){
+    this.estadoService.findAll().subscribe(response => {
+      this.estados = response;
+      this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+      this.updateCidades();
+    },
+    error => {}
+    )
+  }
+
+  updateCidades(){
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id).subscribe(response => {
+      this.cidades = response;
+      this.formGroup.controls.cidadeId.setValue(null);
+    },
+    error => {}
+    );
   }
 
   signupUser(){
