43a204f02f80f36a24f791515e95ef4ab33bf0d9
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-11 20:08:03 -0300

    Implementado o infinite scroll no carregamento dos produtos na tela

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -13,7 +13,8 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -29,9 +30,13 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
-      this.loadImagesUrl();
+    this.produtoService.findByCategoria(categoria_id, this.page, 10).subscribe(response => {
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      let end = this.items.length - 1;
+      console.log(this.page);
+      console.log(this.items);
+      this.loadImagesUrl(start, end);
       loader.dismiss();
     },
     error => {
@@ -39,8 +44,16 @@ export class ProdutosPage {
     });
   }
 
-  loadImagesUrl(){
-    for(var i = 0; i < this.items.length; i++){
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
+
+  loadImagesUrl(start: number, end: number){
+    for(var i = start; i <= end; i++){
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id).subscribe(
         response => {
@@ -65,6 +78,8 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
