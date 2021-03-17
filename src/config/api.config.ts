62ad1d24e9c99f98a77e9d4923b4b0eafa1b3a42
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-16 23:46:17 -0300

    Testado o app no aparelho celular foi feito um deploy no celular

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl:"http://localhost:8081",
+    baseUrl:"https://microlojalaet.herokuapp.com",
+    // baseUrl:"http://localhost:8081",
     bucketBaseUrl:"https://microlojalaet.s3-sa-east-1.amazonaws.com"
 }
