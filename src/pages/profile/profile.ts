ec8705c958c233818ae1080ac2a2ca2ca76aceea
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-17 08:57:12 -0300

    Corrigido bug ao cancelar a captura pela camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -74,6 +74,7 @@ export class ProfilePage {
       this.cameraOn = false;
     }, 
     (err) => {
+      this.cameraOn = false;
     });
   }
 
@@ -93,6 +94,7 @@ export class ProfilePage {
       this.cameraOn = false;
     }, 
     (err) => {
+      this.cameraOn = false;
     });
   }
 
